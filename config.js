faa34bc2d90c812f76d4728a4d09d6450ba4bfe7
savindu8 <119857048+savindu8@users.noreply.github.com>
2023-11-17 14:52:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,8 @@ global.scan  = process.env.QR_URL || 'https://replit.com/@VajiraRathnayak/KING-V
 global.website = 'https://github.com/vajirabot1/KING-VAJIRA-MD' ; //wa.me/+923000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/fb7993e24f7ecdaecbed5.jpg' ;
 module.exports = {
-  sessionName: process.env.SESSION_ID || "VAJIRA;;;DrinkingObsessed",      //Put Your Session Id Here
+  sessionName: process.env.SESSION_ID || "VAJIRA;;;DrinkingObsessed
+",      //Put Your Session Id Here
   botname: process.env.BOT_NAME || 'JAVIS',
   botbgm: process.env.BOT_BGM || 'true',
   ownername:  process.env.OWNER_NAME || 'Buvi',
